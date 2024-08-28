85346289ca4783814b03ad463a0863a27abf46a1
Barry Job M <100874853+Barryjob@users.noreply.github.com>
2024-08-28 23:50:14 +0100

    updated

---
+++ week1/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+age = input("please enter your age here: ")
+
+# location = ("please enter your location here: ")
+
+# print(f"Hello {name}, you are {age} years old and live {location}.")
---
+++ week1/user_input.py
@@ -0,0 +1,8 @@
+name = input("please enter your name: ")
+
+
+age = input("please enter your age here: ")
+
+location = ("please enter your location here: ")
+
+print(f"Hello {name}, you are {age} years old and live {location}.")
